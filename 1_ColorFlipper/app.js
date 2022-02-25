903f878a7ced37ef6df544b4d54e51f9db16faa2
Tim Tsai <81073535+TYTforCoding@users.noreply.github.com>
2022-02-25 15:39:38 +0800

    Add files via upload
    可以成功改變顏色，但是要處理預設值

--- 1_ColorFlipper/app.js
+++ 1_ColorFlipper/app.js
@@ -1,7 +1,12 @@
 // query抓到buttom，監聽click時改變背景顏色與字體顏色
 
 const btn = document.querySelector('button')
+const body = document.querySelector('body')
+const colorName = document.querySelector('#colorName')
 
 btn.addEventListener('click',() => {
-    console.log('click')
+    const randomColor = Math.floor(Math.random()*16777215).toString(16);
+    body.style.backgroundColor= `#${randomColor}`;
+    colorName.innerHTML= `#${randomColor}`;
+    colorName.style.color= `#${randomColor}`;
 })
