e3385c469fa7718f00c33bb9ebc2385229813c03
Tim Tsai <81073535+TYTforCoding@users.noreply.github.com>
2022-02-26 14:09:06 +0800

    Add files via upload
    Add a change color function that only called between -1 ~ 1.
    加入了變色功能，這次想讓功能只有在需要的時候才會被呼叫。

--- 2_Counter/app2_forEach.js
+++ 2_Counter/app2_forEach.js
@@ -6,9 +6,27 @@ const counts = document.querySelector('h2')
 let count = 0
 
 // console.log(btns) 選取到一個陣列內涵3個button
+
 btns.forEach(function(btns) {
     const btn = btns.className;
 
+// 變色function
+        // 想嘗試讓顏色判斷只有在過0的時候會啟動
+        // 因為這個計數器的公差都是1，所以真正需要呼叫變色功能應該只有在 -1,0,1的時候
+        // 因為btn是區域變數 所以必須把function建立在同個scope內
+    const display = function(){
+        if(count <= 1 && count >= -1){
+            // console.log('use function'); 用來確認是否只有在 -1~1之間會呼叫
+            if(count < 0) {
+                counts.style.color = 'red'
+            }else if(count > 0) {
+                counts.style.color = 'green'
+            }else {
+                counts.style.color = '#000'
+            }
+        }
+    }
+// click function
     btns.addEventListener('click',function(){
         if(btn === 'plus'){
             count ++;
@@ -20,6 +38,8 @@ btns.forEach(function(btns) {
             count = 0;
         }
         counts.innerHTML = count;
+        display(count);
     })
-
 });
+
+
