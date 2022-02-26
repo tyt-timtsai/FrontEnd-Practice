db5547d5999718f0074e86733f8c7e1f0ea6cdcf
Tim Tsai <81073535+TYTforCoding@users.noreply.github.com>
2022-02-26 13:24:04 +0800

    Add files via upload
    可以根據數字的正負改變顏色。 再來下一個嘗試要往forEach試試看。

--- 2_Counter/app.js
+++ 2_Counter/app.js
@@ -7,21 +7,43 @@ const reset = document.getElementById('reset')
 const counts = document.querySelector('h2')
 let count = 0
 
+// counter function
+
 plus.addEventListener('click',function() {
     count ++;
     counts.innerHTML = count;
+    display(count)
     // console.log(count);
     return count
 })
 minus.addEventListener('click',function() {
     count --;
     counts.innerHTML = count;
+    display(count)
     // console.log(count);
     return count
 })
 reset.addEventListener('click',function() {
     count = 0;
     counts.innerHTML = count;
+    display(count)
     // console.log(count);
     return count
 })
+
+//display color by number
+//想法 ： 把count與0做大小比較，小於零顯示紅色，大於零顯示綠色
+//考慮 ： 這樣每次按按鈕就要跑一次這個function，感覺比較消耗效能，最好是過了0之後只判斷一次
+
+function display(count) {
+    if (count > 0) { 
+        counts.style.color = '#569043';
+        // console.log('green');
+    }else if (count < 0) {
+        counts.style.color = '#ee3975';
+        // console.log('red');
+    }else {
+        counts.style.color = '#000';
+    }   
+}
+
